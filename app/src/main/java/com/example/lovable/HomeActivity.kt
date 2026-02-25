c470eebd2e6b6649c2ccafe6f39a5a13e3f2ea1a
obaid <obaidurrahmanobaid@gmail.com>
2026-02-26 00:30:43 +0530

    logic code

---
+++ app/src/main/java/com/example/lovable/HomeActivity.kt
@@ -0,0 +1,25 @@
+package com.example.lovable
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_home)
+
+        val btnLogout = findViewById<Button>(R.id.drawerLayout)
+        btnLogout.setOnClickListener {
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
