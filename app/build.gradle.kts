8aa2572540a2ab288a9276d5a492a7be4dcbd2f6
obaid <obaidurrahmanobaid@gmail.com>
2026-03-01 15:28:19 +0530

    logic code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.11.0")
 }
